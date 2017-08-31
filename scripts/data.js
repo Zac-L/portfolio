9248e474b2b8ec19a77b91b34dc451bccf30e1ce
Zac-L <Zachlichens@gmail.com>
2017-08-31 08:58:09 -0700

    added more to constructor function, added media only in style

--- scripts/data.js
+++ scripts/data.js
@@ -2,9 +2,23 @@
 
 var data = [
     {
-        'title': 'About us',
+        'title': 'Salmon Cookies Store',
         'author': 'Zachary Lichens',
         'projectLink': '',
-        'description': ''
+        'description': 'Project about Salmon Cookies'
+    },
+
+    {
+        'title': 'Bus Mall',
+        'author': 'Zachary Lichens',
+        'projectLink': '',
+        'description': 'Project on a Bus Mall Catalog'
+    },
+
+    {
+        'title': 'Quiz Adventure',
+        'author': 'Zachary Lichens',
+        'projectLink': '',
+        'description': 'Project on a Quiz style game'
     }
 ];
