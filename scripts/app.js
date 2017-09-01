b3543cc4cfa4013104f77528c4a40f250a306d55
Zac-L <Zachlichens@gmail.com>
2017-09-01 08:43:11 -0700

    added handlebars

--- scripts/app.js
+++ scripts/app.js
@@ -7,12 +7,21 @@ function Project (inputObj) {
     this.description = inputObj.description;
 }
 
+////Saving for now, till I can get handlebars to work correctly. 
+
+// Project.prototype.toHtml = function() {
+//     var $newProject = $('project.template').clone();
+//     $newProject.removeClass('template');
+
+//     $newProject.find('title').text(this.title);
+//     $newProject.find('projectLink').html(this.projectLink);
+//     $newProject.find('description').text(this.description);
+
+// };
+
 Project.prototype.toHtml = function() {
-    var $newProject = $('project.template').clone();
-    $newProject.removeClass('template');
-
-    $newProject.find('title').text(this.title);
-    $newProject.find('projectLink').html(this.projectLink);
-    $newProject.find('description').text(this.description);
-
+    var source = $('#handle-template').html();
+    var template = Handlebars.compile(source);
+    var html = template(this);
+    return html;
 };
